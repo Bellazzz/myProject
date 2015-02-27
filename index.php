a9162019d8a2d1d450b949a88134b70b8e27e2f2
Mamew <mewseedza@gmail.com>
2015-02-27 18:40:40 +0700

    include Bootstrap + ออกแบบหน้า index

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Test</title>
-	<meta charset="utf8">
-</head>
-<body>
 <?php
-	include('common/class_database.php');
+session_start();
+//require('check_session.php');
+include('config/config.php');
+$tplName = 'index.html';
+$subDir	 = WEB_ROOTDIR.'/';
 
-	// Get record data
-	//$obj = new Titles(139);
-	//echo $obj->getFieldValue('title_name');
-	
-	// Delete record
-	//$obj = new Titles(175);
-	//$obj->delete();
+include('common/common_header.php');
 
-	// Insert all field
-	//$obj = new Titles(array('test insert all field titles'));
-	
-	// Insert specific field
-	//$obj = new Titles(array('title_name'), array('test insert specific field'));
-
-	// Update record
-	//$obj = new Titles(171);
-	//$obj->setFieldValue('title_name', 'test update record');
-	//$obj->commit();
+include('common/common_footer.php');
 ?>
-</body>
-</html>
+
