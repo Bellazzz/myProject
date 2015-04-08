362041942a35914781fcb8d7f0e9c7c3f8986c02
BellLazyLife <supakritsaisin@gmail.com>
2015-04-09 02:02:14 +0700

    รายงานรายการบริการเรียงลำดับตามความนิยม (อัตราการใช้รายการอื่นร่วมกัน)

--- backoffice/report_top_service_lists.php
+++ backoffice/report_top_service_lists.php
@@ -15,7 +15,6 @@ if(isset($_POST['submit'])) {
 	if(isset($_POST['startDate'])) {
 		$startDate = $_POST['startDate'];
 		$smarty->assign('startDate', $startDate);
-		echo $startDate;
 		$smarty->assign('startDate_th', dateThaiFormat($startDate));
 	}
 	if(isset($_POST['endDate'])) {
@@ -26,7 +25,9 @@ if(isset($_POST['submit'])) {
 
 	// Query
 	$report = array();
+	$svls = array();
 	$sql = "SELECT 		sl.svl_name,
+						sl.svl_id,
 						SUM(ss.sersvl_amount) AS amount 
 			FROM 		services s,
 						service_lists sl,
@@ -44,13 +45,72 @@ if(isset($_POST['submit'])) {
 		for($i=0; $i<$rows; $i++) {
 			$record = mysql_fetch_assoc($result);
 
-			array_push($report, array(
+			$report[$record['svl_id']] = array(
 				'no' 		=> number_format($i+1),
+				'svl_id' 	=> $record['svl_id'],
 				'svl_name' 	=> $record['svl_name'],
-				'amount'	=> number_format($record['amount'])
+				'amount'	=> number_format($record['amount']),
+				'together' 	=> array()
+			);
+			array_push($svls, array(
+				'id' => $record['svl_id'],
+				'amount' => $record['amount']
 			));
 
 			$totalAmount += $record['amount'];
+		}
+		
+	}
+
+	// Find percent
+	if(is_array($svls) && count($svls) > 0) {
+		foreach ($svls as $key => $svl) {
+			$svl_id = $svl['id'];
+			$amount = $svl['amount'];
+
+			$sql = "SELECT 		b.svl_id, 
+								b.svl_name, 
+								CONCAT( FORMAT( SUM( b.amount ) /$amount *100, 2 ) ,  '%' ) percent
+					FROM 		(
+								SELECT DISTINCT s.ser_id
+								FROM 			services s, 
+												service_service_lists sl
+								WHERE 			s.ser_date >=  '2015-04-09' AND 
+												s.ser_date <=  '2015-04-09' AND 
+												s.ser_id = sl.ser_id AND 
+												sl.svl_id =  '$svl_id'
+								) a
+								JOIN 
+								(
+								SELECT 		s.ser_id, 
+											sl.svl_id, 
+											sl.svl_name, 
+											SUM( ss.sersvl_amount ) amount
+								FROM 		services s, 
+											service_service_lists ss, 
+											service_lists sl
+								WHERE 		s.ser_date >=  '$startDate' AND 
+											s.ser_date <=  '$endDate' AND 
+											s.ser_id = ss.ser_id AND 
+											ss.svl_id = sl.svl_id AND 
+											sl.svl_id !=  '$svl_id' 
+								GROUP BY 	s.ser_id,sl.svl_id
+								) b ON a.ser_id = b.ser_id 
+					GROUP BY 	b.svl_id 
+					ORDER BY 	SUM( b.amount ) /$amount *100 DESC, b.svl_name 
+					LIMIT 		3";
+			$result = mysql_query($sql, $dbConn);
+			$rows   = mysql_num_rows($result);
+			if($rows > 0) {
+				for($i=0; $i<$rows; $i++) {
+					$record = mysql_fetch_assoc($result);
+
+					array_push($report[$svl_id]['together'], array(
+						'svl_name' => $record['svl_name'],
+						'percent' => $record['percent'],
+					));
+				}
+			}
 		}
 		$smarty->assign('report', $report);
 		$smarty->assign('totalAmount', number_format($totalAmount));
