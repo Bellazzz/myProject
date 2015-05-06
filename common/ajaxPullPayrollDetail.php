cee9e7ec21f3243e00b61713bcf3bd4aaa99bf45
BellLazyLife <supakritsaisin@gmail.com>
2015-05-06 23:43:42 +0700

    BOF การจ่ายเงินเดือน (คำนวณค่าล่วงเวลา ยังไม่ได้คำนวณจากวันหยุดทำงาน จ,อ)

--- common/ajaxPullPayrollDetail.php
+++ common/ajaxPullPayrollDetail.php
@@ -63,6 +63,77 @@ if(hasValue($_POST['emp_id']) && hasValue($_POST['payroll_monthly'])) {
 			$response['sumCommission'] += $record['sumCom'];
 		}
 	}
+
+	// Get work time
+	$otr_otstarttime_daywork = '';
+	$otr_otendtime_daywork = '';
+	$otr_otstarttime_dayoff = '';
+	$otr_otendtime_dayoff = '';
+	$otr_bath_per_hour = null;
+	$sql = "SELECT 		SUBSTRING(ot.otr_otstarttime_daywork,1,5) otr_otstarttime_daywork,
+						SUBSTRING(ot.otr_otendtime_daywork,1,5) otr_otendtime_daywork,
+						SUBSTRING(ot.otr_otstarttime_dayoff,1,5) otr_otstarttime_dayoff,
+						SUBSTRING(ot.otr_otendtime_dayoff,1,5) otr_otendtime_dayoff,
+						ot.otr_bath_per_hour 
+			FROM  		employees e,
+						positions p,
+						overtime_rates ot 
+			WHERE 		e.pos_id = p.pos_id AND 
+						p.otr_id = ot.otr_id AND  
+						e.emp_id = '$emp_id'";
+	$result = mysql_query($sql, $dbConn);
+	$rows 	= mysql_num_rows($result);
+	if($rows > 0) {
+		$record = mysql_fetch_assoc($result);
+		$otr_otstarttime_daywork 	= $record['otr_otstarttime_daywork'];
+		$otr_otendtime_daywork 		= $record['otr_otendtime_daywork'];
+		$otr_otstarttime_dayoff 	= $record['otr_otstarttime_dayoff'];
+		$otr_otendtime_dayoff 		= $record['otr_otendtime_dayoff'];
+		$otr_bath_per_hour 			= $record['otr_bath_per_hour'];
+
+	}
+
+	// Find overtime
+	if($otr_bath_per_hour != null) {
+		$sql = "SELECT 		dateatt_in,
+							timeatt_in,
+							SUBSTRING(timeatt_out,1,5) timeatt_out 
+				FROM 		time_attendances 
+				WHERE 		emp_id = '$emp_id' AND 
+							dateatt_in like '$monthly%'";
+		$result = mysql_query($sql, $dbConn);
+		$rows 	= mysql_num_rows($result);
+		if($rows > 0) {
+			for($i=0; $i<$rows; $i++) {
+				$record = mysql_fetch_assoc($result);
+				$minAttTimeOut = toMin($record['timeatt_out']);
+				$curDay = getSpaCurrentDay($record['dateatt_in']);
+
+				if($curDay == 'วันหยุดสปา') {
+					$minOtrTimeStart = toMin($otr_otstarttime_dayoff);
+					$minOtrTimeEnd = toMin($otr_otendtime_dayoff);
+				} else {
+					$minOtrTimeStart = toMin($otr_otstarttime_daywork);
+					$minOtrTimeEnd = toMin($otr_otendtime_daywork);
+				}
+
+				if($minAttTimeOut >= $minOtrTimeStart) {
+					if($minAttTimeOut > $minOtrTimeEnd)
+						$minAttTimeOut = $minOtrTimeEnd;
+
+					$differ = (int)($minAttTimeOut - $minOtrTimeStart) / 60;
+					$response['sumOvertime'] += $differ * $otr_bath_per_hour;
+				}
+			}
+		} // end for
+	}
+
 } 
 echo json_encode($response);
+
+function toMin($time) {
+	$tmp = split(":", $time);
+	$min = (int)($tmp[0]*60) + $tmp[1];
+	return $min;
+}
 ?>
