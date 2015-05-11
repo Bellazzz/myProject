2dfa0ca5efbbd7db4d8fce4968d9b5984a42eb0f
BellLazyLife <supakritsaisin@gmail.com>
2015-05-12 05:05:33 +0700

    BOF การสำรองข้อมูล (เปลี่ยนเป็น CREATE TABLE IF NOT EXISTS, แก้ไขการ insert ค่าที่เป็น null) >0<

--- backoffice/export.php
+++ backoffice/export.php
@@ -49,17 +49,21 @@ function backup_tables($host,$user,$pass,$name,$tables = '*', $createDB = false)
 		$num_fields = mysql_num_fields($result);
 		
 		
-		$return.= 'DROP TABLE IF EXISTS '.$table.';';
 		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$row2 = str_replace('CREATE TABLE', 'CREATE TABLE IF NOT EXISTS', $row2);
 		$return.= "\n\n".$row2[1].";\n\n";
 		
 		for ($i = 0; $i < $num_fields; $i++)  {
 			while($row = mysql_fetch_row($result)) {
 				$return.= 'INSERT INTO '.$table.' VALUES(';
 				for($j=0; $j<$num_fields; $j++) {
-					$row[$j] = addslashes($row[$j]);
-					$row[$j] = ereg_replace("\n","\\n",$row[$j]);
-					if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+					if(is_null($row[$j])) {
+						$return .= 'NULL';
+					} else {
+						$row[$j] = addslashes($row[$j]);
+						$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+						if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+					}
 					if ($j<($num_fields-1)) { $return.= ','; }
 				}
 				$return.= ");\n";
